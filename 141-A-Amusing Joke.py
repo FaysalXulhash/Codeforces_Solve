3feefc90f8bd44ead0a1a120b94a8ccbd921a257
FaysalXulhash <faysalahammed80@gmail.com>
2024-03-25 11:00:43 +0600

    "solved 141 A amusing-Jokes"

--- 141-A-Amusing Joke.py
+++ 141-A-Amusing Joke.py
@@ -1,12 +1,16 @@
-from collections import Counter
+#from collections import Counter
 a = input()
 b = input()
 s = input()
-a = sorted(a)
-b = sorted(b)
-s = sorted(s)
-c = a+b
-if Counter(c) == Counter(s):
+if sorted(a+b) == sorted(s):
     print('YES')
 else:
     print('NO')
+# a = sorted(a)
+# b = sorted(b)
+# s = sorted(s)
+# c = a+b
+# if Counter(c) == Counter(s):
+#     print('YES')
+# else:
+#     print('NO')
